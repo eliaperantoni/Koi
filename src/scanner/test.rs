b62d7cf9484192d73506a3a0e312734cb348b616
Elia Perantoni <perantonielia0@gmail.com>
2020-11-08 14:51:58 +0100

    More tests

--- src/scanner/test.rs
+++ src/scanner/test.rs
@@ -284,3 +284,201 @@ fn scans_simple_command() {
         },
     ]);
 }
+
+#[test]
+fn scans_mono_string_command() {
+    let scanner = Scanner::new("$(aaa)");
+    assert_eq!(scanner.get_tokens(), vec![
+        Token::String {
+            value: "aaa".to_owned(),
+            does_interp: false,
+            begins_cmd: true,
+            ends_cmd: true,
+        },
+    ]);
+}
+
+#[test]
+fn scans_mono_string_interpolated_command() {
+    let scanner = Scanner::new("$({111})");
+    assert_eq!(scanner.get_tokens(), vec![
+        Token::String {
+            value: "".to_owned(),
+            does_interp: true,
+            begins_cmd: true,
+            ends_cmd: false,
+        },
+        Token::Int {
+            value: 111,
+        },
+        Token::String {
+            value: "".to_owned(),
+            does_interp: false,
+            begins_cmd: false,
+            ends_cmd: true,
+        },
+    ]);
+}
+
+#[test]
+fn scans_command_with_string_literal() {
+    let scanner = Scanner::new("$(a \"b $() b\" c)");
+    assert_eq!(scanner.get_tokens(), vec![
+        Token::String {
+            value: "a".to_owned(),
+            does_interp: false,
+            begins_cmd: true,
+            ends_cmd: false,
+        },
+        Token::String {
+            value: "b $() b".to_owned(),
+            does_interp: false,
+            begins_cmd: false,
+            ends_cmd: false,
+        },
+        Token::String {
+            value: "c".to_owned(),
+            does_interp: false,
+            begins_cmd: false,
+            ends_cmd: true,
+        },
+    ]);
+}
+
+#[test]
+fn scans_command_with_interpolation() {
+    let scanner = Scanner::new("$(a{1}a b{2} b c {3}c d {4} d)");
+    assert_eq!(scanner.get_tokens(), vec![
+        Token::String {
+            value: "a".to_owned(),
+            does_interp: true,
+            begins_cmd: true,
+            ends_cmd: false,
+        },
+        Token::Int {
+            value: 1,
+        },
+        Token::String {
+            value: "a".to_owned(),
+            does_interp: false,
+            begins_cmd: false,
+            ends_cmd: false,
+        },
+        Token::String {
+            value: "b".to_owned(),
+            does_interp: true,
+            begins_cmd: false,
+            ends_cmd: false,
+        },
+        Token::Int {
+            value: 2,
+        },
+        Token::String {
+            value: "".to_owned(),
+            does_interp: false,
+            begins_cmd: false,
+            ends_cmd: false,
+        },
+        Token::String {
+            value: "b".to_owned(),
+            does_interp: false,
+            begins_cmd: false,
+            ends_cmd: false,
+        },
+        Token::String {
+            value: "c".to_owned(),
+            does_interp: false,
+            begins_cmd: false,
+            ends_cmd: false,
+        },
+        Token::String {
+            value: "".to_owned(),
+            does_interp: true,
+            begins_cmd: false,
+            ends_cmd: false,
+        },
+        Token::Int {
+            value: 3,
+        },
+        Token::String {
+            value: "c".to_owned(),
+            does_interp: false,
+            begins_cmd: false,
+            ends_cmd: false,
+        },
+        Token::String {
+            value: "d".to_owned(),
+            does_interp: false,
+            begins_cmd: false,
+            ends_cmd: false,
+        },
+        Token::String {
+            value: "".to_owned(),
+            does_interp: true,
+            begins_cmd: false,
+            ends_cmd: false,
+        },
+        Token::Int {
+            value: 4,
+        },
+        Token::String {
+            value: "".to_owned(),
+            does_interp: false,
+            begins_cmd: false,
+            ends_cmd: false,
+        },
+        Token::String {
+            value: "d".to_owned(),
+            does_interp: false,
+            begins_cmd: false,
+            ends_cmd: true,
+        },
+    ]);
+}
+
+#[test]
+fn scans_command_with_interpolated_string_literal() {
+    let scanner = Scanner::new("$(\"ccc{111}ccc\")");
+    assert_eq!(scanner.get_tokens(), vec![
+        Token::String {
+            value: "ccc".to_owned(),
+            does_interp: true,
+            begins_cmd: true,
+            ends_cmd: false,
+        },
+        Token::Int {
+            value: 111,
+        },
+        Token::String {
+            value: "ccc".to_owned(),
+            does_interp: false,
+            begins_cmd: false,
+            ends_cmd: true,
+        },
+    ]);
+}
+
+#[test]
+fn scans_command_with_interpolated_command() {
+    let scanner = Scanner::new("$({$(a)})");
+    assert_eq!(scanner.get_tokens(), vec![
+        Token::String {
+            value: "".to_owned(),
+            does_interp: true,
+            begins_cmd: true,
+            ends_cmd: false,
+        },
+        Token::String {
+            value: "a".to_owned(),
+            does_interp: false,
+            begins_cmd: true,
+            ends_cmd: true,
+        },
+        Token::String {
+            value: "".to_owned(),
+            does_interp: false,
+            begins_cmd: false,
+            ends_cmd: true,
+        },
+    ]);
+}
