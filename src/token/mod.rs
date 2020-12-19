caddcbe936dea5ec776fc2b4c17684b75a4c3980
Elia Perantoni <perantonielia0@gmail.com>
2020-12-19 14:35:02 +0100

    Tokens

---
+++ src/token/mod.rs
@@ -0,0 +1,96 @@
+#[derive(Debug)]
+pub struct Token {
+    pub kind: TokenKind,
+    pub lexeme: String,
+}
+
+#[derive(PartialEq, Debug, Clone)]
+pub enum TokenKind {
+    Identifier {
+        name: String,
+    },
+    Num {
+        value: f64,
+    },
+    String {
+        value: String,
+        does_interp: bool,
+    },
+
+    For,
+    While,
+    If,
+    Else,
+    Fn,
+    Return,
+    Break,
+    Continue,
+    Var,
+    Exp,
+
+    LeftParen,
+    RightParen,
+    LeftBracket,
+    RightBracket,
+    LeftBrace,
+    RightBrace,
+
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+
+    PlusEqual,
+    MinusEqual,
+    StarEqual,
+    SlashEqual,
+    CaretEqual,
+    PercEqual,
+
+    Great,
+    GreatEqual,
+    Less,
+    LessEqual,
+
+    Plus,
+    Minus,
+    Star,
+    Slash,
+    Caret,
+    Perc,
+
+    PlusPlus,
+    MinusMinus,
+
+    True,
+    False,
+    Nil,
+
+    AmperAmper,
+    PipePipe,
+
+    Comma,
+    Dot,
+    Colon,
+
+    // Commands related stuff ...
+
+    Pipe,
+    StarPipe,
+    AmperPipe,
+
+    StarGreat,
+    AmperGreat,
+
+    StarLess,
+    AmperLess,
+
+    Semicolon,
+
+    // ... until here
+
+    Space,
+    Newline,
+
+    Eof,
+}
