9b9efc07926beec4a7292f20965efc93e9b007a0
Elia Perantoni <perantonielia0@gmail.com>
2021-01-04 19:43:34 +0100

    Dummy `parse_stmt` implementation

--- src/parser/stmt.rs
+++ src/parser/stmt.rs
@@ -1,7 +1,10 @@
-use crate::ast::{Expr, Stmt};
+use crate::ast::{Value, Expr, Stmt};
 use crate::token::{Token, TokenKind};
 
 use super::Parser;
 
 impl Parser {
+    pub fn parse_stmt(&mut self) -> Stmt {
+        Stmt::Expr(Expr::Literal(Value::Nil))
+    }
 }
