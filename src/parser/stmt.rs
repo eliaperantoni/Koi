742e0d6cb88bea6791fbed2ece3c184b35f158b0
Elia Perantoni <perantonielia0@gmail.com>
2021-01-06 12:10:05 +0100

    Stmt parsing!

--- src/parser/stmt.rs
+++ src/parser/stmt.rs
@@ -1,10 +1,60 @@
-use crate::ast::{Value, Expr, Stmt};
+use itertools::Itertools;
+
+use crate::ast::{Expr, Stmt, Value};
 use crate::token::{Token, TokenKind};
 
 use super::Parser;
 
 impl Parser {
     pub fn parse_stmt(&mut self) -> Stmt {
-        Stmt::Expr(Expr::Literal(Value::Nil))
+        if self.is_expr_next() {
+            let expr = self.parse_expression(0);
+            if !matches!(expr, Expr::Set(..) | Expr::SetField {..} | Expr::Call {..}) {
+                panic!("only assignment and call expressions are allowed as statements");
+            }
+            Stmt::Expr(expr)
+        } else {
+            let cmd = self.parse_cmd();
+            Stmt::Cmd(cmd)
+        }
+    }
+
+    fn is_expr_next(&mut self) -> bool {
+        if !self.lexer.is_new_line {
+            return true;
+        }
+
+        self.lexer.start_recording();
+
+        let line_tokens = self.lexer
+            .by_ref()
+            .take_while(|t| t.kind != TokenKind::Newline)
+            .filter(|t| ![TokenKind::Space, TokenKind::Newline].contains(&t.kind))
+            .collect::<Vec<Token>>();
+
+        self.lexer.stop_recording(true);
+
+        let mut line_tokens_iter = line_tokens.iter().peekable();
+
+        loop {
+            if !matches!(line_tokens_iter.next(), Some(Token {kind: TokenKind::Identifier(..), ..})) {
+                return false;
+            }
+
+            if matches!(line_tokens_iter.peek(), Some(&Token {kind: TokenKind::Dot, ..})) {
+                line_tokens_iter.next();
+                continue;
+            }
+
+            if matches!(line_tokens_iter.next(),
+                Some(Token {kind: TokenKind::LeftParen, ..}) |
+                Some(Token {kind: TokenKind::LeftBracket, ..}) |
+                Some(Token {kind: TokenKind::Equal, ..})
+            ) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
