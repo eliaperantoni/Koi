4e73dd76b324ef6f0d67a6b97e0b5200faadb581
Elia Perantoni <perantonielia0@gmail.com>
2021-01-06 13:22:22 +0100

    More parser tests

--- src/parser/test.rs
+++ src/parser/test.rs
@@ -12,6 +12,10 @@ fn parse_expression(source: &str) -> Expr {
     make_parser(source).parse_expression(0)
 }
 
+fn parse(source: &str) -> Vec<Stmt> {
+    make_parser(source).parse()
+}
+
 #[test]
 fn parses_literals() {
     for (source, want) in &[
@@ -121,4 +125,82 @@ fn parses_associativity() {
         BinaryOp::Sum,
         Box::new(Expr::Literal(Value::Num(3.0))),
     ));
+}
+
+#[test]
+fn parses_cmd_stmt() {
+    assert_eq!(parse("cmd1\ncmd2"), vec![
+        Stmt::Cmd(vec![vec![Expr::Literal(Value::String("cmd1".to_owned()))]]),
+        Stmt::Cmd(vec![vec![Expr::Literal(Value::String("cmd2".to_owned()))]]),
+    ]);
+}
+
+#[test]
+fn parses_assignment_stmt() {
+    assert_eq!(parse("foo = \n    1"), vec![
+        Stmt::Expr(Expr::Set("foo".to_owned(), Box::new(Expr::Literal(Value::Num(1.0)))))
+    ]);
+}
+
+#[test]
+fn parses_call_stmt() {
+    assert_eq!(parse("my_fn(\n    \"foo\",\n    \"bar\"\n)"), vec![
+        Stmt::Expr(Expr::Call {
+            func: Box::new(Expr::Get("my_fn".to_owned())),
+            args: vec![
+                Expr::Literal(Value::String("foo".to_owned())),
+                Expr::Literal(Value::String("bar".to_owned())),
+            ],
+        }),
+    ]);
+}
+
+#[test]
+fn parses_cmd_stmt_with_dot() {
+    assert_eq!(parse("cmd.exe"), vec![
+        Stmt::Cmd(vec![vec![Expr::Literal(Value::String("cmd.exe".to_owned()))]]),
+    ]);
+}
+
+#[test]
+fn parses_assignment_stmt_with_dots() {
+    assert_eq!(parse("x.foo = 1"), vec![
+        Stmt::Expr(Expr::SetField {
+            base: Box::new(Expr::Get("x".to_owned())),
+            index: Box::new(Expr::Literal(Value::String("foo".to_owned()))),
+            value: Box::new(Expr::Literal(Value::Num(1.0))),
+        }),
+    ]);
+}
+
+#[test]
+fn parses_call_stmt_with_dots() {
+    assert_eq!(parse("x.foo()"), vec![
+        Stmt::Expr(Expr::Call {
+            func: Box::new(Expr::GetField {
+                base: Box::new(Expr::Get("x".to_owned())),
+                index: Box::new(Expr::Literal(Value::String("foo".to_owned()))),
+            }),
+            args: vec![],
+        })
+    ]);
+}
+
+#[test]
+fn parses_incorrect_expr_stmt_with_dots() {
+    assert_eq!(parse("x.foo\n()"), vec![
+        Stmt::Cmd(vec![vec![Expr::Literal(Value::String("x.foo".to_owned()))]]),
+        Stmt::Cmd(vec![vec![Expr::Literal(Value::String("()".to_owned()))]]),
+    ]);
+}
+
+#[test]
+fn parses_expr_stmt_continuation() {
+    assert_eq!(parse("foo() foo\n=1"), vec![
+        Stmt::Expr(Expr::Call {
+            func: Box::new(Expr::Get("foo".to_owned())),
+            args: vec![],
+        }),
+        Stmt::Expr(Expr::Set("foo".to_owned(), Box::new(Expr::Literal(Value::Num(1.0))))),
+    ]);
 }
