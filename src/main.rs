3ff3c4ae509420f8e432e7ee87b3534fe15c98ec
Elia Perantoni <perantonielia0@gmail.com>
2021-03-26 15:26:20 +0100

    Koifile support

--- src/main.rs
+++ src/main.rs
@@ -21,16 +21,36 @@ fn main() {
     let matches = App::new("Koi")
         .version("1.0.0")
         .author("Elia Perantoni <perantonielia0@gmail.com>")
-        .arg(Arg::with_name("PATH").help("Path to soure file. If omitted will read from STDIN."))
+        .arg(
+            Arg::with_name("path")
+                .value_name("PATH")
+                .index(1)
+                .takes_value(true)
+                .help("Path to source file.")
+        )
+        .arg(
+            Arg::with_name("stdin")
+                .short("s")
+                .long("stdin")
+                .takes_value(false)
+                .help("Read script from stdin.")
+                .conflicts_with("path")
+        )
+        .arg(
+            Arg::with_name("fn")
+                .short("f")
+                .long("--fn")
+                .takes_value(true)
+                .help("Function to call.")
+        )
         .get_matches();
 
-    let source = match matches.value_of("PATH") {
-        Some(path) => fs::read_to_string(path).unwrap(),
-        None => {
-            let mut buffer = String::new();
-            io::stdin().read_to_string(&mut buffer).unwrap();
-            buffer
-        }
+    let source = if matches.is_present("stdin") {
+        let mut buffer = String::new();
+        io::stdin().read_to_string(&mut buffer).unwrap();
+        buffer
+    } else {
+        fs::read_to_string(matches.value_of("path").unwrap_or("Koifile")).unwrap()
     };
 
     let lexer = new_lexer(source);
@@ -40,4 +60,15 @@ fn main() {
 
     let mut interpreter = interp::Interpreter::new();
     interpreter.run(prog);
+
+    if let Some(f) = matches.value_of("fn") {
+        use ast::{Stmt, Expr};
+
+        interpreter.run(vec![
+            Stmt::Expr(Expr::Call {
+                func: Box::new(Expr::Get(f.to_string())),
+                args: vec![]
+            })
+        ]);
+    }
 }
